1e515e8433c3135b63faa7667d2acbe4bbbea78f
qwerty <Kehsa754@gmail.com>
2024-07-18 16:01:37 +0300

    ssert

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,9 +1,7 @@
-from capitalize import capitalize
+from auto_test.capitalize import capitalize
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
+assert capitalize('hello') == 'Hello'
 
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
+assert capitalize('') == ''
 
 print('Все тесты пройдены!')
